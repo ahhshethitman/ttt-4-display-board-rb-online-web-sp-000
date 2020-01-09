897d81e0d16da802d7cec6b9d066f400b3e8df77
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-09 19:06:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,4 +12,10 @@ game_board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(game_board)
   
+puts (" game_board#{0},|game_board#{1},|game_board#{2}")
+puts ("-----------")
+puts ("   |   |   ")
+puts ("-----------")
+puts ("   |   |   ")
+  
 end
