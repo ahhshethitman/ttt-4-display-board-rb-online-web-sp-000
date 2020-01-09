f1e51a312c847610ba962b740645bd1e0abe2d91
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-09 20:59:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-game_board = [" "," "," "," "," "," "," "," "," "]
+game_board = [" "," "," "," ","X"," "," "," "," "]
 
 #("   |   |   ")
 #("-----------")
