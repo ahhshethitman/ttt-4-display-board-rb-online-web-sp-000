604bd2673cbdaca2e4c78a68edcd57328e79d81a
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-09 19:04:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,14 @@
 # out the current state.
 
 game_board = []
- expect(rows[0]).to eq("   |   |   ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
+
+("   |   |   ")
+("-----------")
+("   |   |   ")
+("-----------")
+("   |   |   ")
+
+
 def display_board(game_board)
   
 end
